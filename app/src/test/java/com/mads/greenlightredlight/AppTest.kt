3d4eda7824cd868ee5a00993208881e7830629e6
maddy-daniel <madelynd@student.fdu.edu>
2026-06-09 12:48:37 -0400

    MAD-45: Finished Implementing Junit tests for Rollover

--- app/src/test/java/com/mads/greenlightredlight/AppTest.kt
+++ app/src/test/java/com/mads/greenlightredlight/AppTest.kt
@@ -40,7 +40,7 @@ class RolloverTest{
         val lastWeekSunday = getMostRecentSunday(today)
         val lastRolloverStr = lastWeekSunday.format(dateFormatter)
         val result = shouldRollover(today, lastRolloverStr)
-        assertTrue("Rollover should NOT happen when last rollover was this Sunday", result)
+        assertFalse("Rollover should NOT happen when last rollover was this Sunday", result)
     }
 
     @Test
@@ -48,7 +48,46 @@ class RolloverTest{
     fun rolloverShouldHappenWhenLastRolloverWasLastWeek(){
         val today = LocalDate.now()
         val lastWeekSunday = getMostRecentSunday(today).minusWeeks(1)
-
-
+        val lastRolloverStr = lastWeekSunday.format(dateFormatter)
+        val result = shouldRollover(today, lastRolloverStr)
+        assertTrue("Rollover should happen when last rollover was last week",result)
     }
+
+    @Test
+    //Tests when rollover should happen when the last rollover was months ago
+    fun rolloverShouldHappenWhenLastRolloverWasMonthsAgo(){
+        val today = LocalDate.now()
+        val monthsAgo = today.minusMonths(3)
+        val lastRolloverStr = monthsAgo.format(dateFormatter)
+        val result = shouldRollover(today, lastRolloverStr)
+        assertTrue("Rollover should happen when last rollover was months ago", result)
+    }
+
+    @Test
+    //Tests when rollover should not happen when the rollover was today
+    fun rolloverShouldNotHappenWhenLastRolloverWasToday(){
+        val today = LocalDate.now()
+        val lastRolloverStr = today.format(dateFormatter)
+        val result = shouldRollover(today, lastRolloverStr)
+        assertFalse("Rollover should NOT happen when last rollover was today.", result)
+    }
+
+    @Test
+    //Tests that the recent Sunday is correct based on a given date
+    fun mostRecentSundayCalculationCorrect(){
+        val monday = LocalDate.of(2026,6,1)
+        val expectedSunday = LocalDate.of(2026,5,31)
+        val result = getMostRecentSunday(monday)
+        assertEquals("Most recent Sunday from Monday, June 1st should be May 31st", expectedSunday, result)
+    }
+
+    @Test
+    //Tests that if the date is a Sunday then it should be the new Recent Sunday
+    fun mostRecentSundayWhenTodayIsSunday(){
+        val sunday = LocalDate.of(2026,6,7)
+        val result = getMostRecentSunday(sunday)
+        assertEquals("Most recent Sunday when today is Sunday should return today's date", sunday, result)
+    }
+
+
 }
